9c3ac1bd8a9753c727f67a55ea34dd3ff94f6fe7
Leandro Rodrigues <leandroberro@hotmail.com>
2017-05-09 16:28:18 -0300

    Iniciate map and AI files

---
+++ mapInterface.c
@@ -0,0 +1,6 @@
+// Código para fazer interface com o tabuleiro atual
+
+// Gerar mapa (código exemplo)
+// Retornar mapa
+// Aplicar movimento e retornar mapa novo
+// Guardar até quantas interações do mapa anteriores?
---
+++ noiceAi.c
@@ -0,0 +1,6 @@
+// Código da IA que faz as buscas e checa
+
+// Ler mapa e por num grafo?
+// Fazer força bruta primeiro?
+// Heurística 1: ...;
+// Heurística 2: ...;
